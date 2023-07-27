33dbc40ed81f269271240bbcc0e763df4ff9041c
Artemiy <rendrom@gmail.com>
2023-07-27 17:42:41 +0200

    FIXUP attributes tab name

--- nextgisweb/feature_layer/nodepkg/attribute-editor/AttributeEditorStore.ts
+++ nextgisweb/feature_layer/nodepkg/attribute-editor/AttributeEditorStore.ts
@@ -55,7 +55,7 @@ class AttributeEditorStore implements EditorStore<NgwAttributeValue> {
 
     reset = () => {
         this.load(this._initValue);
-    }
+    };
 
     setValues = (values: AppAttributes = {}) => {
         runInAction(() => {
--- nextgisweb/feature_layer/nodepkg/attribute-editor/index.ts
+++ nextgisweb/feature_layer/nodepkg/attribute-editor/index.ts
@@ -1,11 +1,11 @@
 import AttributeEditorStore from "./AttributeEditorStore";
 
-import i18n from "@nextgisweb/pyramid/i18n";
+import { gettext } from "@nextgisweb/pyramid/i18n";
 
 import type { EditorWidgetRegister } from "@nextgisweb/feature-layer/type";
 import type { NgwAttributeValue } from "./type";
 
-const titleText = i18n.gettext("Attachments");
+const titleText = gettext("Attributes");
 
 const editorWidgetRegister: EditorWidgetRegister<NgwAttributeValue> = {
     component: () => import("./AttributeEditor"),
